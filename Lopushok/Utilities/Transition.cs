329d929f61834a4a7cf93e353686be14099e4536
Владислав <utkinvaa@gmail.com>
2022-01-26 11:05:37 +0300

    Добавьте файлы проекта.

---
+++ Lopushok/Utilities/Transition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lopushok.Entities;
+using System.Windows.Controls;
+
+namespace Lopushok.Utilities
+{
+    internal class Transition
+    {
+        public static Frame mainFrame;
+        private static LopushokEntities _context { get; set; }
+        public static LopushokEntities Context
+        {
+            get
+            {
+                if (_context == null)
+                    _context = new LopushokEntities();
+                return _context;
+            }
+        }
+    }
+}
