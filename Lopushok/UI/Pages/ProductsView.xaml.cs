f51e42efd80a77f03164fb8181ef1fedd7714f14
Владислав <utkinvaa@gmail.com>
2022-01-26 13:41:17 +0300

    Реализованы поиск, сортировка и фильтрация данных

--- Lopushok/UI/Pages/ProductsView.xaml.cs
+++ Lopushok/UI/Pages/ProductsView.xaml.cs
@@ -23,8 +23,6 @@ namespace Lopushok.UI.Pages
     public partial class ProductsView : Page
     {
         public List<ProductMaterial> ItemsProductMaterial { get { return Transition.Context.ProductMaterial.ToList(); } }
-        public int countItemsProductMaterial { get { return ItemsProductMaterial.Count; } }
-        ViewPages vp = new ViewPages(0, 1);
         public ProductsView()
         {
             InitializeComponent();
@@ -39,10 +37,56 @@ namespace Lopushok.UI.Pages
             sortByCombox.Items.Insert(2, "По наименованию");
             sortByCombox.SelectedIndex = 0;
 
-            vp.itemsCount = countItemsProductMaterial;
-            vp.GetIndex();
 
-            listViewProducts.ItemsSource = ItemsProductMaterial.GetRange(vp.startIndex, vp.rangeItemsCount);
+            listViewProducts.ItemsSource = ItemsProductMaterial;
+        }
+
+        public void UpdateData()
+        {
+            var updatedData = Transition.Context.ProductMaterial.ToList();
+            if (searchTxt.Text != "Поиск..." && searchTxt.Text != "")
+            {
+                updatedData = updatedData.Where(x => x.Product.Title.ToLower().Contains(searchTxt.Text.ToLower()) ||
+                                                 x.Material.Title.ToLower().Contains(searchTxt.Text.ToLower())).ToList();
+            }
+            if (filterByCombox.SelectedIndex > 0)
+            {
+                updatedData = updatedData.Where(p => p.Product.ProductType.Title == (filterByCombox.SelectedItem as ProductType).Title.ToString()).ToList();
+            }
+            if (sortByCombox.SelectedIndex > 0)
+            {
+                switch (sortByCombox.SelectedIndex)
+                {
+                    case 1:
+                        {
+                            if (ascDescCheck.IsChecked == false)
+                            {
+                                updatedData = updatedData.OrderBy(x => x.Material.Cost).ToList();
+                            }
+                            else
+                            {
+                                updatedData = updatedData.OrderByDescending(x => x.Material.Cost).ToList();
+                            }
+
+                            break;
+                        }
+                    case 2:
+                        {
+                            if (ascDescCheck.IsChecked == false)
+                            {
+                                updatedData = updatedData.OrderBy(x => x.Product.Title).ToList();
+                            }
+                            else
+                            {
+                                updatedData = updatedData.OrderByDescending(x => x.Product.Title).ToList();
+                            }
+                            break;
+                        }
+                }
+
+
+            }
+            listViewProducts.ItemsSource = updatedData;
         }
 
         private void addBtn_Click(object sender, RoutedEventArgs e)
@@ -58,6 +102,46 @@ namespace Lopushok.UI.Pages
         private void deleteBtn_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (searchTxt.Text == "Поиск...")
+                searchTxt.Text = "";
+        }
+
+        private void searchTxt_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (searchTxt.Text == "")
+                searchTxt.Text = "Поиск...";
+        }
+
+        private void searchTxt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (searchTxt.Text != "Поиск..." && searchTxt.Text != "")
+            {
+                UpdateData();
+            }
+        }
+
+        private void sortByCombox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void filterByCombox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            UpdateData();
         }
     }
 }
